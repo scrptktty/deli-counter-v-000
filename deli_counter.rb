234d6d736be3ad9d3924d60dfd8a751d7095e28f
Kachel Wittig <kjwittig@gmail.com>
2018-03-12 23:06:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,12 +10,23 @@ def line(katz_deli)
     katz_deli.each_with_index do |person_in_line, index|
       line_number = index + 1
       deli_list = "#{line_number}. #{person_in_line}"
-      binding.pry
       deli_line << deli_list
     end
+    binding.pry
     "The line is currently:" + deli_line.join(" ")
   end
 end
+#
+# def assign_rooms(attendees)
+#   welcome_rooms = []
+#   attendees.each_with_index do |attendee, index|
+#     room = index + 1
+#     room_list = "Hello, #{attendee}! You'll be assigned to room #{room}!"
+#     welcome_rooms << room_list
+#   end
+#   return welcome_rooms
+# end
+
 
 def take_a_number
 end
