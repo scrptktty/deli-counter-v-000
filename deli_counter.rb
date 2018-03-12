9b9427ed2960c482076a00cb7adce6f584a8bb2b
Kachel Wittig <kjwittig@gmail.com>
2018-03-12 23:11:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,6 +12,7 @@ def line(katz_deli)
       deli_list = "#{line_number}. #{person_in_line}"
       deli_line << deli_list
     end
+    binding.pry
     "The line is currently: " + deli_line.join(" ")
   end
 end
