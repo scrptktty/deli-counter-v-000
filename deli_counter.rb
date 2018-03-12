6812e674973309261138556dd63feade0c6e16b8
Kachel Wittig <kjwittig@gmail.com>
2018-03-12 23:44:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ end
 def take_a_number(katz_deli, name)
   katz_deli.each_with_index do |person_in_line, index|
     line_number = index + 1
-    welcome = "Welcome, #{name}. You are number #{line_number} in line."
+    puts "Welcome, #{name}. You are number #{line_number} in line."
     binding.pry
   end
 end
