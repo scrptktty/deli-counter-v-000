ca34f24396c508c7e68d6fefc60554c00c5fdeb0
Kachel Wittig <kjwittig@gmail.com>
2018-03-12 23:01:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+require 'pry'
+
+katz_deli = []
+
+def line(katz_deli)
+  deli_line = []
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |person_in_line, index|
+      line_number = index + 1
+      deli_list = "#{line_number}. #{person_in_line}"
+      deli_line << deli_list
+    end
+    puts "The line is currently:" + deli_line.join(" ")
+  end
+end
+
+def take_a_number
+end
+
+def now_serving
+end
