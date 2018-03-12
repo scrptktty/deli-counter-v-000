510ddf7d4f4e5f8bd4dfdc2396707cddd60358df
Kachel Wittig <kjwittig@gmail.com>
2018-03-12 23:30:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,9 +17,9 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  while katz_deli.!empty?
     number = (line(katz_deli).length) + 1
     puts "Welcome, #{name}. You are number #{number} in line."
+  else
   end
 end
 
